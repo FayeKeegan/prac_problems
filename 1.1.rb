d74b607792de197b1c464a79e998d7bb0f15ead9
Faye Keegan <faye.keegan@gmail.com>
2015-10-13 13:28:09 -0700

    initial commit

---
+++ 1.1.rb
@@ -0,0 +1,19 @@
+def array_flip
+end
+
+def rotate_matrix(arry)
+	n = arry.length
+	layer = 0
+	while layer < array.length / 2
+		first = layer
+	layer += 1
+	end
+
+end
+
+matr1 = [[3, 0, 1],
+				 [0, 2, 1],
+				 [0, 0, 1]]
+
+
+puts diag_mirror(matr1).to_s
