bd9ba4cc17c61d94c74dccbcdc5f73046c1f672b
Faye Keegan <faye.keegan@gmail.com>
2015-10-13 19:41:09 -0700

    sum linked list, basically working

--- 2.rb
+++ 2.rb
@@ -23,6 +23,10 @@ class SinglyLinkedList
 	end
 
 	def push_bottom(node)
+		if @head.nil?
+			@head = node
+			@tail = node
+		end
 		prev_tail = @tail
 		prev_tail.next_node = node
 		@tail = node
@@ -35,19 +39,28 @@ class SinglyLinkedList
 	end
 
 	def delete_node(node)
-		if self.head == node
-			@head = @head.next_node
-		else 
-			check_node = self.head
-			found = false
-			until check_node.nil? || found
-				if check_node.next_node == node
-					check_node.next_node = check_node.next_node.next_node
-					found = true
-				end
-				check_node = check_node.next_node
-			end
+		#bad solution
+		# if self.head == node
+		# 	@head = @head.next_node
+		# else 
+		# 	check_node = self.head
+		# 	found = false
+		# 	until check_node.nil? || found
+		# 		if check_node.next_node == node
+		# 			check_node.next_node = check_node.next_node.next_node
+		# 			found = true
+		# 		end
+		# 		check_node = check_node.next_node
+		# 	end
+		# end
+
+		#better solution, but wont work for tail
+		if (node == self.head)
+			self.head == node.next_node
 		end
+		node.data = node.next_node.data
+		node.next_node = node.next_node.next_node
+		node
 	end
 
 	def to_s
@@ -104,25 +117,69 @@ class SinglyLinkedList
 		end
 	end
 
+
+end
+
+def sum_linked_lists(list1, list2)
+	#set up starting node with value of 0, at head of sum_list
+	starting_node = Node.new(0)
+	sum_list = SinglyLinkedList.new
+	sum_list.push_bottom(starting_node)
+
+	#start with the first three nodes that you want to sum
+	starting_node = sum_list.head
+	curr_node_1 = list1.head
+	curr_node_2 = list2.head
+
+	until curr_node_1.nil? && curr_node_2.nil?
+		#get three numbers to sum
+		starting_sum = starting_node.data
+		num_1 = curr_node_1 ? curr_node_1.data : 0
+		num_2 = curr_node_2 ? curr_node_2.data : 0
+		current_sum = starting_sum + num_1 + num_2
+		
+		#update current sum node with the correct sum
+		starting_node.data = current_sum % 10
+
+		#add next node to sum node depending on sum
+		current_sum >= 10 ? sum_list.push_bottom(Node.new(1)) : sum_list.push_bottom(Node.new(0))
+		
+
+		#increment each node by 1
+		starting_node = starting_node.next_node
+		curr_node_1 = curr_node_1.nil? ? nil : curr_node_1.next_node
+		curr_node_2 = curr_node_2.nil? ? nil : curr_node_2.next_node
+	end
+	#this basically works but has a floating 0 at the end sometimes... should rewrite recursively
+	sum_list
 end
 
 #singly linked list
 
 #create nodes
-n1 = Node.new(1)
-n2 = Node.new(2)
-n3 = Node.new(3)
-n_dup = Node.new(2)
-n4 = Node.new(4)
-
-#link them
-n1.next_node = n2
-n2.next_node = n3
-n3.next_node = n_dup
-n_dup.next_node = n4
+nodes = [Node.new(1), Node.new(2), Node.new(3), Node.new(2), Node.new(4)]
 
 #make a list
-s_list = SinglyLinkedList.new(n1, n4)
+s_list = SinglyLinkedList.new
+nodes.each do |node| s_list.push_bottom(node) end
 puts s_list.to_s
+
+#practice with sums
+
+#create numbers
+array_109 = [Node.new(9), Node.new(0), Node.new(1)]
+array_182 = [Node.new(2), Node.new(8), Node.new(1)]
+list_109 = SinglyLinkedList.new()
+list_182 = SinglyLinkedList.new()
+array_109.each do |node|
+	list_109.push_bottom(node)
+end
+array_182.each do |node|
+	list_182.push_bottom(node)
+end
+
+
+
+
 
 
