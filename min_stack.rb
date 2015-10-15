1bdf810f65300a41a4cbb0f5cdb2a96bbf7c9437
Faye Keegan <faye.keegan@gmail.com>
2015-10-15 12:20:52 -0700

    sort of working min stack

--- min_stack.rb
+++ min_stack.rb
@@ -1,13 +1,28 @@
 class MinStack
 
 def initialize
-	@min = nil
 	@store = []
 end
 
 def pop
-
+	return nil if @store.empty?
+	prev_top = @store.pop[0]
+	@min = @store.last[1]
+	@top = @store.last[0]
+	prev_top
 end
+
+def min
+	@store.last[1]
+end
+
+
+def add(el)
+	prev_min = @store.last[1]
+	new_min = prev_min < el ? prev_min : el
+	@store.push()
+end
+
 
 
 
